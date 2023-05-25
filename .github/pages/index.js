0f63ab3f8065cecfbeae9eb47262a60c465def53
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-05-25 11:10:37 +0100

    Update Home Component

--- .github/pages/index.js
+++ .github/pages/index.js
@@ -1,89 +1,85 @@
-import { useState } from "react";
-import Head from "next/head";
-import Image from "next/image";
+import { useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default function Home() {
-  const [prediction, setPrediction] = useState(null);
-  const [error, setError] = useState(null);
+	const [prediction, setPrediction] = useState(null);
+	const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("/api/predictions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: e.target.prompt.value,
-      }),
-    });
-    let prediction = await response.json();
-    if (response.status !== 201) {
-      setError(prediction.detail);
-      return;
-    }
-    setPrediction(prediction);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const response = await fetch('/api/predictions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				prompt: e.target.prompt.value,
+			}),
+		});
+		let prediction = await response.json();
+		if (response.status !== 201) {
+			setError(prediction.detail);
+			return;
+		}
+		setPrediction(prediction);
 
-    while (
-      prediction.status !== "succeeded" &&
-      prediction.status !== "failed"
-    ) {
-      await sleep(1000);
-      const response = await fetch("/api/predictions/" + prediction.id);
-      prediction = await response.json();
-      if (response.status !== 200) {
-        setError(prediction.detail);
-        return;
-      }
-      console.log({ prediction });
-      setPrediction(prediction);
-    }
-  };
+		while (prediction.status !== 'succeeded' && prediction.status !== 'failed') {
+			await sleep(1000);
+			const response = await fetch('/api/predictions/' + prediction.id);
+			prediction = await response.json();
+			if (response.status !== 200) {
+				setError(prediction.detail);
+				return;
+			}
+			console.log({ prediction });
+			setPrediction(prediction);
+		}
+	};
 
-  return (
-    <div className="container max-w-2xl mx-auto p-5">
-      <Head>
-        <title>Replicate + Next.js</title>
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Replicate + Next.js</title>
+			</Head>
 
-      <h1 className="py-6 text-center font-bold text-2xl">
-        Dream something with{" "}
-        <a href="https://replicate.com/stability-ai/stable-diffusion">
-          Stable Diffusion
-        </a>
-      </h1>
+			<p>
+				Dream something with{' '}
+				<a href='https://replicate.com/stability-ai/stable-diffusion'>
+					stability-ai/stable-diffusion
+				</a>
+				:
+			</p>
 
-      <form className="w-full flex" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="flex-grow"
-          name="prompt"
-          placeholder="Enter a prompt to display an image"
-        />
-        <button className="button" type="submit">
-          Go!
-        </button>
-      </form>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<input
+					type='text'
+					name='prompt'
+					placeholder='Enter a prompt to display an image'
+				/>
+				<button type='submit'>Go!</button>
+			</form>
 
-      {error && <div>{error}</div>}
+			{error && <div>{error}</div>}
 
-      {prediction && (
-        <>
-          {prediction.output && (
-            <div className="image-wrapper mt-5">
-              <Image
-                fill
-                src={prediction.output[prediction.output.length - 1]}
-                alt="output"
-                sizes="100vw"
-              />
-            </div>
-          )}
-          <p className="py-3 text-sm opacity-50">status: {prediction.status}</p>
-        </>
-      )}
-    </div>
-  );
+			{prediction && (
+				<div>
+					{prediction.output && (
+						<div className={styles.imageWrapper}>
+							<Image
+								fill
+								src={prediction.output[prediction.output.length - 1]}
+								alt='output'
+								sizes='100vw'
+							/>
+						</div>
+					)}
+					<p>status: {prediction.status}</p>
+				</div>
+			)}
+		</div>
+	);
 }
